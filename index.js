83039276bc704d4489cfc0ffd72f5705cda9655b
Benjo <ben.ohman@gokstadakademiet.no>
2024-01-17 21:32:03 +0100

    js selection ok

---
+++ index.js
@@ -0,0 +1,7 @@
+userInput=document.getElementById("user-input");
+searchBtn=document.getElementById("search-btn");
+
+apiName=document.getElementById("api-name");
+apiCategory=document.getElementById("api-category");
+apiAlcoholic=document.getElementById("api-alcoholic");
+apiInstr=document.getElementById("api-instr");
